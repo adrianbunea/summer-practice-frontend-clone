ee348fa3624c5aae7a881b8ec3d550ac9c47044d
adrianbunea <adrian.bunea28@gmail.com>
2023-06-19 12:29:38 +0300

    Add initial Freaks List page

---
+++ src/pages/all/components/freak/index.jsx
@@ -0,0 +1,7 @@
+import "./index.css";
+
+export const Freak = ({ freak }) => (
+    <div className="freak">
+        {freak.name}
+    </div>
+)
--- src/pages/all/index.jsx
+++ src/pages/all/index.jsx
@@ -1,3 +1,30 @@
+import { Freak } from "./components/freak"
+import "./index.css"
+
 export const All = () => {
-    return <h1>All</h1>
+    const freaks = [
+        { name: 'Adi' },
+        { name: 'Sandra' },
+        { name: 'Calin' },
+        { name: 'Georgiana' },
+        { name: 'Anda' },
+        { name: 'Adi' },
+        { name: 'Sandra' },
+        { name: 'Calin' },
+        { name: 'Georgiana' },
+        { name: 'Anda' },
+        { name: 'Adi' },
+        { name: 'Sandra' },
+        { name: 'Calin' },
+        { name: 'Georgiana' },
+        { name: 'Anda' }
+    ]
+
+    return (
+        <div className="page">
+            <div className="freaks-list">
+                {freaks.map(freak => <Freak freak={freak} />)}
+            </div>
+        </div>
+    ) 
 }
