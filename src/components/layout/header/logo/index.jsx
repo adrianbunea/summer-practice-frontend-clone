f306d8a1b2f1bc5ddfba96749e09d8221562d424
adrianbunea <adrian.bunea28@gmail.com>
2023-07-03 14:24:27 +0300

    Fix logo

--- src/components/layout/header/logo/index.jsx
+++ src/components/layout/header/logo/index.jsx
@@ -1,5 +1,6 @@
 import "./index.css"
+import logo from './logo.png'
 
 export const Logo = () => (
-    <img src="/public/logo.png" alt="Logo of Agile Freaks Evil Incorporated" className="logo"/>
+    <img src={logo} alt="Logo of Agile Freaks Evil Incorporated" className="logo"/>
 )
