ef2f3ba32504c26cc510eb2fe19719c038f4f3fc
adrianbunea <adrian.bunea28@gmail.com>
2023-07-03 14:04:06 +0300

    Fix warnings

--- src/components/layout/header/logo/index.jsx
+++ src/components/layout/header/logo/index.jsx
@@ -1,5 +1,5 @@
 import "./index.css"
 
 export const Logo = () => (
-    <img src="/public/logo.png" className="logo"/>
+    <img src="/public/logo.png" alt="Logo of Agile Freaks Evil Incorporated" className="logo"/>
 )
