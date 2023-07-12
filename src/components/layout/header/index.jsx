a069a1e9721a0b026fa11e693a5cb898e5de8249
adrianbunea <adrian.bunea28@gmail.com>
2023-07-12 17:10:13 +0300

    Make app responsive

--- src/components/layout/header/index.jsx
+++ src/components/layout/header/index.jsx
@@ -1,12 +1,22 @@
+import {useState} from "react";
+
 import "./index.css"
 import { Logo } from './logo'
 import { Navigation } from './navigation'
+import {MobileNavigation} from "./mobile-navigation";
+import {NavigationToggle} from "./navigation-toggle";
 
 export const Header = () => {
+    const [navigationOpen, setNavigationOpen] = useState(false);
+    const toggleNavigation = () => setNavigationOpen(open => !open)
+
+
     return (
         <header className="header">
             <Logo />
+            <NavigationToggle onClick={toggleNavigation}/>
             <Navigation />
+            <MobileNavigation open={navigationOpen} onClickOutside={() => setNavigationOpen(false)}/>
         </header>
     )
 }
