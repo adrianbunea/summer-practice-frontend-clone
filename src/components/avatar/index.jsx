5d6c1c56f97099096624eaf4a4caaf7bd0bfe62a
biancamariacampean <bianca.campean96@gmail.com>
2023-07-12 15:10:13 +0300

    Create navigation bar

--- src/components/avatar/index.jsx
+++ src/components/avatar/index.jsx
@@ -1,5 +1,5 @@
 import "./index.css";
 
 export const Avatar = ({ src, self = false }) => (
-    <></>
+    <img src={src} alt={'Avatar'} className={`avatar ${self && '--self'} `} />
 )
--- src/components/layout/header/index.jsx
+++ src/components/layout/header/index.jsx
@@ -1,10 +1,12 @@
 import "./index.css"
 import { Logo } from './logo'
+import { Navigation } from './navigation'
 
 export const Header = () => {
     return (
         <header className="header">
             <Logo />
+            <Navigation />
         </header>
     )
 }
