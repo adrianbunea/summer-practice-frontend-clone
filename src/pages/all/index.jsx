510b9bb3851be8d4e9fde9a532be8878590733de
biancamariacampean <bianca.campean96@gmail.com>
2023-07-12 15:41:46 +0300

    Display a list of freaks

--- src/pages/all/index.jsx
+++ src/pages/all/index.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react"
+import { getFreaks } from './get-freaks';
 import "./index.css"
 
-export const All = () => (
-    <></>
-)
+export const All = () => {
+    const [freaks, setFreaks] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const onSuccess = freaks => {
+        setFreaks(freaks);
+        setLoading(false);
+    }
+
+    const onFailure = error => {
+        setError(error);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        getFreaks(onSuccess, onFailure)
+    }, []);
+
+    return (
+        <div className="page">
+            <div className="freak-list">
+                {!loading ?
+                    freaks.map((freak, index) => (
+                        <div className="freak-list__cell" key={index}>
+                            <p>{freak.attributes.first_name}</p>
+                        </div>
+                    ))
+                : 'Loading...' }
+            </div>
+        </div>
+    )
+}
